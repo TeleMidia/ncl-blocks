10f9922c38cbbfcb738fb259b46a7e172eda55b5
Alan Livio <alan@telemidia.puc-rio.br>
2017-02-15 17:12:13 -0200

    Change srgs and ssml blocks to mutators.

--- ncl-blocks.js
+++ ncl-blocks.js
@@ -3,16 +3,16 @@ goog.require('Blockly.Blocks');
 // entities
 
 Blockly.Blocks['media'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("--media--");
+      .setAlign(Blockly.ALIGN_CENTRE)
+      .appendField("--media--");
     this.appendDummyInput()
-        .appendField("id")
-        .appendField(new Blockly.FieldTextInput(""), "id");
+      .appendField("id")
+      .appendField(new Blockly.FieldTextInput(""), "id");
     this.appendValueInput("src")
-        .setCheck("media_content")
-        .appendField("conteúdo");
+      .setCheck("media_content")
+      .appendField("conteúdo");
     this.setInputsInline(false);
     this.setColour(120);
     this.setTooltip('Modalidade de saida');
@@ -21,16 +21,16 @@ Blockly.Blocks['media'] = {
 };
 
 Blockly.Blocks['input'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("--input-- ");
+      .setAlign(Blockly.ALIGN_CENTRE)
+      .appendField("--input-- ");
     this.appendDummyInput()
-        .appendField("id")
-        .appendField(new Blockly.FieldTextInput(""), "id");
+      .appendField("id")
+      .appendField(new Blockly.FieldTextInput(""), "id");
     this.appendValueInput("src")
-        .setCheck("input_content")
-        .appendField("conteúdo");
+      .setCheck("input_content")
+      .appendField("conteúdo");
     this.setInputsInline(false);
     this.setColour(120);
     this.setTooltip('Modalidade de entrada');
@@ -39,16 +39,16 @@ Blockly.Blocks['input'] = {
 };
 
 Blockly.Blocks['user'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("--user--");
+      .setAlign(Blockly.ALIGN_CENTRE)
+      .appendField("--user--");
     this.appendDummyInput()
-        .appendField("id")
-        .appendField(new Blockly.FieldTextInput(""), "id");
+      .appendField("id")
+      .appendField(new Blockly.FieldTextInput(""), "id");
     this.appendValueInput("NAME")
-        .setCheck("user_content")
-        .appendField("src");
+      .setCheck("user_content")
+      .appendField("src");
     this.setColour(20);
     this.setTooltip('');
     this.setHelpUrl('');
@@ -58,9 +58,9 @@ Blockly.Blocks['user'] = {
 // contents
 
 Blockly.Blocks['image'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldImage("media/icon-image.png", 50, 50, "*"));
+      .appendField(new Blockly.FieldImage("media/icon-image.png", 50, 50, "*"));
     this.setOutput(true, "media_content");
     this.setColour(120);
     this.setTooltip('');
@@ -72,16 +72,67 @@ Blockly.Blocks['srgs'] = {
   init: function () {
     this.appendDummyInput().appendField(
       new Blockly.FieldImage("media/srgs.png", 50, 50, "*"));
-    this.appendDummyInput().appendField('<grammar>')
-    this.appendDummyInput().appendField('  <rule id="')
-      .appendField(new Blockly.FieldTextInput(""), "id")
-      .appendField('">')
-      .appendField(new Blockly.FieldTextInput('repetir o video'), "")
-    this.appendDummyInput().appendField('</grammar>')
     this.setColour(120);
-    this.setOutput(true, "input_content");
-    this.setTooltip('');
-    this.setHelpUrl('');
+    this.itemCount_ = 1;
+    this.updateShape_();
+    this.setOutput(true, 'simplecondition');
+    this.setMutator(new Blockly.Mutator(['lists_create_with_item']));
+  },
+  mutationToDom: function () {
+    var container = document.createElement('mutation');
+    container.setAttribute('items', this.itemCount_);
+    return container;
+  },
+  domToMutation: function (xmlElement) {
+    this.itemCount_ = parseInt(xmlElement.getAttribute('items'), 10);
+    this.updateShape_();
+  },
+  decompose: function (workspace) {
+    var containerBlock = workspace.newBlock('lists_create_with_container');
+    containerBlock.initSvg();
+    var connection = containerBlock.getInput('STACK').connection;
+
+    for (var i = 0; i < this.itemCount_; i++) {
+      var itemBlock = workspace.newBlock('lists_create_with_item');
+      itemBlock.initSvg();
+      connection.connect(itemBlock.previousConnection);
+      connection = itemBlock.nextConnection;
+    }
+    return containerBlock;
+  },
+  compose: function (containerBlock) {
+    var itemBlock = containerBlock.getInputTargetBlock('STACK');
+    var connections = [];
+
+    while (itemBlock) {
+      connections.push(itemBlock.valueConnection_);
+      itemBlock = itemBlock.nextConnection &&
+        itemBlock.nextConnection.targetBlock();
+    }
+    this.itemCount_ = connections.length;
+    this.updateShape_();
+  },
+  saveConnections: function (containerBlock) {},
+
+  updateShape_: function () {
+    if (this.itemCount_ && this.getInput('EMPTY')) {
+      this.removeInput('EMPTY');
+    } else if (!this.itemCount_ && !this.getInput('EMPTY')) {
+      this.appendDummyInput('EMPTY')
+    }
+    for (var i = 0; i < this.itemCount_; i++) {
+      if (!this.getInput('ADD' + i)) {
+        this.appendDummyInput('ADD' + i)
+          .appendField("ancora id")
+          .appendField(new Blockly.FieldTextInput(""), "id")
+          .appendField("reconhecer ")
+          .appendField(new Blockly.FieldTextInput('repetir o video'), "")
+      }
+    }
+    while (this.getInput('ADD' + i)) {
+      this.removeInput('ADD' + i);
+      i++;
+    }
   }
 };
 
@@ -89,17 +140,67 @@ Blockly.Blocks['ssml'] = {
   init: function () {
     this.appendDummyInput().appendField(
       new Blockly.FieldImage("media/ssml.png", 50, 50, "*"));
-    this.appendDummyInput().appendField('<speak>')
-    this.appendDummyInput().appendField('  <s id="')
-      .appendField(new Blockly.FieldTextInput(""), "id").appendField('">')
-      .appendField(
-        new Blockly.FieldTextInput('Você gostaria de repetir o video?'), "")
-      .appendField('</s>')
-    this.appendDummyInput().appendField('</speak>');
     this.setColour(120);
-    this.setOutput(true, "media_content");
-    this.setTooltip('');
-    this.setHelpUrl('');
+    this.itemCount_ = 1;
+    this.updateShape_();
+    this.setOutput(true, 'simplecondition');
+    this.setMutator(new Blockly.Mutator(['lists_create_with_item']));
+  },
+  mutationToDom: function () {
+    var container = document.createElement('mutation');
+    container.setAttribute('items', this.itemCount_);
+    return container;
+  },
+  domToMutation: function (xmlElement) {
+    this.itemCount_ = parseInt(xmlElement.getAttribute('items'), 10);
+    this.updateShape_();
+  },
+  decompose: function (workspace) {
+    var containerBlock = workspace.newBlock('lists_create_with_container');
+    containerBlock.initSvg();
+    var connection = containerBlock.getInput('STACK').connection;
+
+    for (var i = 0; i < this.itemCount_; i++) {
+      var itemBlock = workspace.newBlock('lists_create_with_item');
+      itemBlock.initSvg();
+      connection.connect(itemBlock.previousConnection);
+      connection = itemBlock.nextConnection;
+    }
+    return containerBlock;
+  },
+  compose: function (containerBlock) {
+    var itemBlock = containerBlock.getInputTargetBlock('STACK');
+    var connections = [];
+
+    while (itemBlock) {
+      connections.push(itemBlock.valueConnection_);
+      itemBlock = itemBlock.nextConnection &&
+        itemBlock.nextConnection.targetBlock();
+    }
+    this.itemCount_ = connections.length;
+    this.updateShape_();
+  },
+  saveConnections: function (containerBlock) {},
+
+  updateShape_: function () {
+    if (this.itemCount_ && this.getInput('EMPTY')) {
+      this.removeInput('EMPTY');
+    } else if (!this.itemCount_ && !this.getInput('EMPTY')) {
+      this.appendDummyInput('EMPTY')
+    }
+    for (var i = 0; i < this.itemCount_; i++) {
+      if (!this.getInput('ADD' + i)) {
+        this.appendDummyInput('ADD' + i)
+          .appendField("ancora id")
+          .appendField(new Blockly.FieldTextInput(""), "id")
+          .appendField("sintetizar")
+          .appendField(new Blockly.FieldTextInput('repetir o video'), "")
+      }
+    }
+    while (this.getInput('ADD' + i)) {
+      this.removeInput('ADD' + i);
+      i++;
+    }
   }
 };
 
@@ -157,11 +258,13 @@ Blockly.Blocks['video'] = {
     }
     for (var i = 0; i < this.itemCount_; i++) {
       if (!this.getInput('ADD' + i)) {
-        var input = this.appendDummyInput('ADD' + i);
-        input.appendField("ancora id").appendField(
-            new Blockly.FieldTextInput(""), "id").appendField("inicia em")
-          .appendField(new Blockly.FieldNumber(0, 0), "begin").appendField(
-            "e termina em").appendField(new Blockly.FieldNumber(0, 0), "end")
+        this.appendDummyInput('ADD' + i)
+          .appendField("ancora id")
+          .appendField(new Blockly.FieldTextInput(""), "id")
+          .appendField("inicia em")
+          .appendField(new Blockly.FieldNumber(0, 0), "begin")
+          .appendField("e termina em")
+          .appendField(new Blockly.FieldNumber(0, 0), "end")
           .appendField("s");
       }
     }
@@ -175,11 +278,18 @@ Blockly.Blocks['video'] = {
 // behavior
 
 Blockly.Blocks['simplecondition'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["iniciar","onBegin"], ["terminar","onEnd"], ["atribuir","onEndAttribution"], ["reconhecer","onRecognize"]]), "role")
-        .appendField("a")
-        .appendField(new Blockly.FieldDropdown([["","OPTIONNAME"]]), "NAME");
+      .appendField(new Blockly.FieldDropdown([
+        ["iniciar", "onBegin"],
+        ["terminar", "onEnd"],
+        ["atribuir", "onEndAttribution"],
+        ["reconhecer", "onRecognize"]
+      ]), "role")
+      .appendField("a")
+      .appendField(new Blockly.FieldDropdown([
+        ["", "OPTIONNAME"]
+      ]), "NAME");
     this.setInputsInline(false);
     this.setOutput(true, "simplecondition");
     this.setColour(260);
@@ -189,13 +299,13 @@ Blockly.Blocks['simplecondition'] = {
 };
 
 Blockly.Blocks['behavior'] = {
-  init: function() {
+  init: function () {
     this.appendValueInput("conditions")
-        .setCheck(["compoundcondition", "simplecondition"])
-        .appendField("quando");
+      .setCheck(["compoundcondition", "simplecondition"])
+      .appendField("quando");
     this.appendStatementInput("actions")
-        .setCheck("simpleaction")
-        .appendField("faça");
+      .setCheck("simpleaction")
+      .appendField("faça");
     this.setInputsInline(false);
     this.setColour(260);
     this.setTooltip('');
@@ -204,11 +314,17 @@ Blockly.Blocks['behavior'] = {
 };
 
 Blockly.Blocks['simpleaction'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["inicie","start"], ["pare","stop"], ["continue","resume"]]), "action")
-        .appendField("a")
-        .appendField(new Blockly.FieldDropdown([["","OPTIONNAME"]]), "NAME");
+      .appendField(new Blockly.FieldDropdown([
+        ["inicie", "start"],
+        ["pare", "stop"],
+        ["continue", "resume"]
+      ]), "action")
+      .appendField("a")
+      .appendField(new Blockly.FieldDropdown([
+        ["", "OPTIONNAME"]
+      ]), "NAME");
     this.setPreviousStatement(true, "simpleaction");
     this.setNextStatement(true, "simpleaction");
     this.setColour(230);
