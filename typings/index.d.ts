fb8c779ce617f28be1cc0f059c3aba3b807831e1
Alan Livio <alan@telemidia.puc-rio.br>
2017-03-26 20:26:37 -0300

    Enable typing support and disable unused npm dependencies.

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/survey-knockout/index.d.ts" />
+/// <reference path="../nclblocks/nclblocks.js" />
+/// <reference path="../node_modules/blockly/blockly_compressed.js" />
+/// <reference path="../node_modules/blockly/blocks_compressed.js" />
