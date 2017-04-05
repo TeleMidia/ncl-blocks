1936bf036ac9499667a5384d7b5507317d822272
Alan Livio <alan@telemidia.puc-rio.br>
2017-04-05 14:31:13 -0300

    Use dxsurvey builder code. Add conclude message.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,187 @@
-var surveyJSON = { pageNextText: "seguir", pages: [{ name: "termo", questions: [{ type: "html", name: "termo", html: "O TeleMídia, Grupo de Pesquisa do Departamento de informática da PUC-Rio, desenvolve pesquisa científica nas áreas de Sistemas Multimídia/Hipermídia e Comunicação de Dados Multimídia. Em particular, Sistemas de TV Digital, terrestre e IPTV, têm sido foco de pesquisa e inovação do laboratório nos últimos anos. <br>\n<br>\nConvidamos a você a participar de um de nossos estudos. \nTrata-se da avaliação de uma pesquisa intitulada de \"novas interações de usuario em linguagens multimedia\". <br>\nEla NÃO se destinar a avaliar seus conhecimentos e sim para avaliar o resultado de nossa pesquisa.<br>\n<br>\n<br>\nPara prosseguirmos, pedimos seu consentimento para realizar a avaliação descrita acima, fornecendo a você algumas informações adicionais.<br>\n- Os dados coletados destinam-se estritamente a atividades de pesquisa e desenvolvimento, e somente os pesquisadores do TeleMídia têm acesso à integra do material resultante da avaliação.<br>\n- A divulgação dos resultados de nossa pesquisa em foros científicos e/ou pedagógicos pauta-se no respeito à privacidade, e o anonimato dos participantes é preservado em quaisquer materiais que elaboramos.<br>\n<br>\n<br> " }, { type: "text", name: "nome", title: "Caso esteja de acordo, insira seu nome no campo a seguir e prossiga." }], title: "Termo de concentimento" }, { name: "perfil", questions: [{ type: "radiogroup", name: "experiencia NCL", title: "Qual sua experiencia com a linguagem NCL?", choices: [{ value: "1", text: "1 (nenhuma)" }, "2", "3", "4", "5", "6", { value: "7", text: "7 (expert)" }] }, { type: "radiogroup", name: "apps NCL", title: "Quantas aplicações NCL você desenvolveu?", choices: [{ value: "1", text: "1-2" }, { value: "2", text: "3-4" }, { value: "3", text: "5-6" }, { value: "4", text: "6-7" }, { value: "5", text: "7-8" }, { value: "6", text: "8-9" }, { value: "7", text: "10 ou mais" }] }, { type: "radiogroup", name: "experiencia HTML", title: "Qual sua experiencia com a linguagem HTML/JavaScript?", choices: [{ value: "1", text: "1 (nenhuma)" }, "2", "3", "4", "5", "6", { value: "7", text: "7 (expert)" }] }, { type: "radiogroup", name: "apps HTML", title: "Quantas aplicações HTML/JavaScript você desenvolveu?", choices: [{ value: "1", text: "1-2" }, { value: "2", text: "3-4" }, { value: "3", text: "5-6" }, { value: "4", text: "6-7" }, { value: "5", text: "7-8" }, { value: "6", text: "8-9" }, { value: "7", text: "10 ou mais" }] }], title: "Perfil" }, { name: "interações multimodais em autoria multimídia", questions: [{ type: "html", name: "question1" }], title: "introdução a linguagens multimídia" }, { name: "NCL com interações multimodais", questions: [{ type: "html", name: "question2" }] }, { name: "HTML com interações multimodais", questions: [{ type: "html", name: "question3" }] }, { name: "aceitação", questions: [{ type: "html", name: "question4" }], title: "Avaliação final" }], requiredText: "", showProgressBar: "bottom", showQuestionNumbers: "off", surveyPostId: "51d57b85-3813-4a08-801b-4b7e077c1660", title: "Avalição de suporte a interações multimodais em linguagens multimídia" }
+var surveyJSON = {
+  completeText: "Finalizar estudo",
+  completedHtml: "<div class=\"alert alert-success\" role=\"alert\">\n  <h3>Muito bem!!!\n  Obrigado por sua participação em nosso estudo.</h3>\n<span class=\"glyphicon glyphicon-thumbs-up\" aria-hidden=\"true\"></span>\n</div>",
+  locale: "en",
+  pageNextText: "seguir",
+  pagePrevText: "voltar",
+  pages: [
+    {
+      name: "termo",
+      questions: [
+        {
+          type: "html",
+          name: "termo",
+          html: "O TeleMídia, Grupo de Pesquisa do Departamento de Informática da PUC-Rio, desenvolve pesquisa científica nas áreas de Sistemas Multimídia/Hipermídia e Comunicação de Dados Multimídia. Em particular, Sistemas de TV Digital, terrestre e IPTV, têm sido foco de pesquisa e inovação do laboratório nos últimos anos. \n<br><br>\n\nConvidamos a você a participar de um de nossos estudos. \nTrata-se da avaliação de uma pesquisa intitulada de \"novas interações de usuario em linguagens multimedia\".\n<br>\n<br>\n\nEla NÃO se destinar a avaliar seus conhecimentos e sim para avaliar o resultado de nossa pesquisa.\n<br>\n<br>\n\nPara prosseguirmos, pedimos seu consentimento para realizar a avaliação descrita acima, fornecendo a você algumas informações adicionais:\n<ul>\n<li>Os dados coletados destinam-se estritamente a atividades de pesquisa e desenvolvimento, e somente os pesquisadores do TeleMídia têm acesso à integra do material resultante da avaliação.</li>\n<br>\n<li>A divulgação dos resultados de nossa pesquisa em foros científicos e/ou pedagógicos pauta-se no respeito à privacidade, e o anonimato dos participantes é preservado em quaisquer materiais que elaboramos.</li>\n</ul>\n<br>\n<br>"
+        },
+        {
+          type: "text",
+          name: "nome",
+          title: "Caso esteja de acordo, insira seu nome no campo a seguir e prossiga."
+        }
+      ],
+      title: "Termo de concentimento"
+    },
+    {
+      name: "perfil",
+      questions: [
+        {
+          type: "radiogroup",
+          name: "experiencia NCL",
+          title: "Qual sua experiencia com a linguagem NCL?",
+          choices: [
+            {
+              value: "1",
+              text: "1 (nenhuma)"
+            },
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            {
+              value: "7",
+              text: "7 (expert)"
+            }
+          ]
+        },
+        {
+          type: "radiogroup",
+          name: "apps NCL",
+          title: "Quantas aplicações NCL você desenvolveu?",
+          choices: [
+            {
+              value: "1",
+              text: "1-2"
+            },
+            {
+              value: "2",
+              text: "3-4"
+            },
+            {
+              value: "3",
+              text: "5-6"
+            },
+            {
+              value: "4",
+              text: "6-7"
+            },
+            {
+              value: "5",
+              text: "7-8"
+            },
+            {
+              value: "6",
+              text: "8-9"
+            },
+            {
+              value: "7",
+              text: "10 ou mais"
+            }
+          ]
+        },
+        {
+          type: "radiogroup",
+          name: "experiencia HTML",
+          title: "Qual sua experiencia com a linguagem HTML/JavaScript?",
+          choices: [
+            {
+              value: "1",
+              text: "1 (nenhuma)"
+            },
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            {
+              value: "7",
+              text: "7 (expert)"
+            }
+          ]
+        },
+        {
+          type: "radiogroup",
+          name: "apps HTML",
+          title: "Quantas aplicações HTML/JavaScript você desenvolveu?",
+          choices: [
+            {
+              value: "1",
+              text: "1-2"
+            },
+            {
+              value: "2",
+              text: "3-4"
+            },
+            {
+              value: "3",
+              text: "5-6"
+            },
+            {
+              value: "4",
+              text: "6-7"
+            },
+            {
+              value: "5",
+              text: "7-8"
+            },
+            {
+              value: "6",
+              text: "8-9"
+            },
+            {
+              value: "7",
+              text: "10 ou mais"
+            }
+          ]
+        }
+      ],
+      title: "Perfil"
+    },
+    {
+      name: "interações multimodais em autoria multimídia",
+      questions: [
+        {
+          type: "html",
+          name: "question1"
+        }
+      ],
+      title: "introdução a linguagens multimídia"
+    },
+    {
+      name: "NCL com interações multimodais",
+      questions: [
+        {
+          type: "html",
+          name: "question2"
+        }
+      ]
+    },
+    {
+      name: "HTML com interações multimodais",
+      questions: [
+        {
+          type: "html",
+          name: "question3"
+        }
+      ]
+    },
+    {
+      name: "aceitação",
+      questions: [
+        {
+          type: "html",
+          name: "question4"
+        }
+      ],
+      title: "Avaliação final"
+    }
+  ],
+  requiredText: "",
+  showProgressBar: "top",
+  showQuestionNumbers: "off",
+  surveyPostId: "51d57b85-3813-4a08-801b-4b7e077c1660",
+  title: "Estudo sobre interações multimodais em linguagens multimídia"
+}
 
 function blockly_inject_sq_100() {
 
