996b07ba85e40c36037d41ac9813fc5a7b9c7ef2
Alan Livio <alan@telemidia.puc-rio.br>
2017-06-30 21:32:24 -0300

    Remove unused vscode tasks.json and typings.

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/survey-knockout/index.d.ts" />
-/// <reference path="../src/nclblocks/nclblocks.js" />
-/// <reference path="../node_modules/blockly/blockly_compressed.js" />
-/// <reference path="../node_modules/blockly/blocks_compressed.js" />
