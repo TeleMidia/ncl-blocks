107670eb83fefb291a0c4729a1f45c503543d18f
Alan Livio <alan@telemidia.puc-rio.br>
2017-07-28 02:39:27 -0300

    multimodal-survey: Use blockly genUid for survey user. Retry send until sucess.

--- examples/multimodal-survey/index.js
+++ examples/multimodal-survey/index.js
@@ -43,18 +43,12 @@ var _blocksTask4WorkspaceEdited = false
 // survey create
 // ----------------------------------------
 
-function uuidv4 () {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8)
-    return v.toString(16)
-  })
-}
-var _userId = uuidv4()
 var _postId = '51d57b85-3813-4a08-801b-4b7e077c1660'
-var _sendTries = 3
+var _userId = Blockly.utils.genUid()
+console.log(_userId)
 
 function onSendResult (survey, options) {
-  if (!options.success && _sendTries-- > 0) {
+  if (!options.success) {
     setTimeout(function () {
       console.log('sendResult')
       survey.sendResult(_postId, _userId)
